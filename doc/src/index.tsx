9b3e08eb45004150913dfa371833bcc858543a39
Maxime Dréau <maxime@dreau.fr>
2024-08-28 17:03:26 +0200

    Utilisation du HashRouter pour faire fonctionner les liens GH Pages

--- doc/src/index.tsx
+++ doc/src/index.tsx
@@ -2,13 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* GitHub Pages does not support SPA without hacks so we use the hash router */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
