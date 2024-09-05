c22c17b02cd8fe6736887894106f6c7ecfaa5753
Maxime Dréau <maxime@dreau.fr>
2024-09-05 10:42:38 +0200

    Revert "Utilisation du HashRouter pour faire fonctionner les liens GH Pages"
    This reverts commit 9b3e08eb45004150913dfa371833bcc858543a39.

--- doc/src/index.tsx
+++ doc/src/index.tsx
@@ -2,14 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    {/* GitHub Pages does not support SPA without hacks so we use the hash router */}
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
